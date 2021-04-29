d056a6d3478d3f7f860dd45f6ee405d90afdc285
Carlo Sala <carlosalag@protonmail.com>
2021-04-29 20:54:46 +0200

    no plot si return diferent de 0

--- dissonance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gcc -Wall -pedantic -o dissonance dissonance.c -lm
-./dissonance
-gnuplot -p -e "plot 'diss.txt' using 1:2 with lines title 'nostre'"
-
---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+gcc -Wall -pedantic -o dissonance dissonance.c -lm
+./dissonance
+if [[ $? -eq 0 ]]; then
+  gnuplot -p -e "plot 'diss.txt' using 1:2 with lines title 'nostre'"
+fi
