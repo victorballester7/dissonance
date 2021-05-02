25daa07878165a9d566cf233128addcfe8297333
Carlo Sala <carlosalag@protonmail.com>
2021-05-02 14:20:54 +0200

    esborrat using 1:2 plot

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 gcc -Wall -pedantic -o dissonance dissonance.c -lm
 ./dissonance
 if [[ $? -eq 0 ]]; then
-  gnuplot -p -e "plot 'diss.txt' using 1:2 with lines"
+  gnuplot -p -e "plot 'diss.txt' with lines"
 fi
