01f97b1cfd633fbbc8b473ca52e72fd3915b0760
Victor Ballester <victor.ballester.ribo@gmail.com>
2021-05-03 12:15:50 +0200

    el ratio de la grafica arribava fins a 1.998 i no 2

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 gcc -Wall -pedantic -o dissonance dissonance.c -lm
 ./dissonance
 if [[ $? -eq 0 ]]; then
-  gnuplot -p -e "plot 'diss.txt' with lines"
+  gnuplot -p -e "plot [1:2] 'diss.txt' with lines"
 fi
