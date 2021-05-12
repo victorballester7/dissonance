662ca585086b575ae973e6c4c9411db9c8a6cd73
Victor Ballester <victor.ballester.ribo@gmail.com>
2021-05-12 14:20:48 +0200

    algun canvi

--- dissonance.c
+++ dissonance.c
@@ -18,7 +18,7 @@ int main() {
   printf("Introduir 1 si es vol calcular la grafica per a sons simples.\nIntroduir 2 si es vol calcular la grafica per a sons complexos.\nIntroduir 3 si es vol calcular el valor de la dissonancia entre 2 sons.\n");
   scanf("%i", &n);
   int harmonics = 7, harmonics_F1 = 1, harmonics_F2 = 1, punts = 500; //harmonics = nombre d'harmonics, punts = punts del grafic.
-  double r_max = 2, r = 1, potencia = 0.4, f1, f2, F1[harmonics][2], F2[harmonics][2]; //r_max = maxim ratio entre les dues frequencies i potencia es el valor k tal que l'harmonic i-essim te amplitud 1/i^k. F1[harmonics][2] = llista d'harmonics. A la primera columna la freqüència; a la segona, l'amplitud.
+  double r_max = 2, r = 1, potencia = 0.4, f1, f2; //r_max = maxim ratio entre les dues frequencies i potencia es el valor k tal que l'harmonic i-essim te amplitud 1/i^k. F1[harmonics][2] = llista d'harmonics. A la primera columna la freqüència; a la segona, l'amplitud.
   switch (n) {
     case 1://So simple (taula per graficar).
       printf("Introduir una frequencia base:\n");
@@ -32,28 +32,30 @@ int main() {
         fprintf(fitxer, "%.16G %.16G\n", r, disso_simple(f1, r * f1, 1, 1));
       fclose(fitxer);
       break;
-    case 2: //So complex (taula per graficar).
+    case 2:{ //So complex (taula per graficar).
       printf("Introduir una frequencia base:\n");
       scanf("%lf", &f1);
       if (f1 < MIN_FREQ || f1 > MAX_FREQ) {
         printf("Error! La frequencia es menor a 20 Hz o major a 20000 Hz.\n");
         return 1;
       }
-      while ((harmonics_F1 + 1) * f1 <= MAX_FREQ) harmonics_F1++; //afegeix tants harmonics com sigui possible de f1 (sense superar "harmonics" a "harmonics_F1")
-      nota_musical(f1, harmonics, potencia, F1);
+      while ((harmonics_F1 + 1) * f1 <= MAX_FREQ) harmonics_F1++; //afegeix tants harmonics com sigui possible de f1 (sense superar "harmonics") a "harmonics_F1"
+      double F1[harmonics_F1][2];
+      nota_musical(f1, harmonics_F1, potencia, F1);
       fitxer = fopen("diss.txt", "w");
       if (fitxer == NULL) {
         printf("No s'ha pogut crear el fitxer.");
         return 1;
       }
       for (int k = 0; k <= punts; k++, r += (r_max - 1)/punts) {
-        while ((harmonics_F2 + 1) * r * f1 <= MAX_FREQ) harmonics_F2++; //afegeix tants harmonics com sigui possible de f1 (sense superar "harmonics" a "harmonics_F2")
+        while ((harmonics_F2 + 1) * r * f1 <= MAX_FREQ) harmonics_F2++; //afegeix tants harmonics com sigui possible de f1 (sense superar "harmonics") a "harmonics_F2"
+        double F2[harmonics_F2][2];
         nota_musical(r * f1, harmonics_F2, potencia, F2);
         fprintf(fitxer, "%.16G %.16G\n", r, disso_complexa(harmonics_F1, harmonics_F2, F1, F2));
       }
       fclose(fitxer);
-      break;
-    case 3: //Valor de dissonacia entre dos sons introduits manualment.
+      break;}
+    case 3:{ //Valor de dissonacia entre dos sons introduits manualment.
       printf("Introduir la frequencia fonamental del so 1: ");
       scanf("%lf", &f1);
       printf("Introduir la frequencia fonamental del so 2: ");
@@ -64,10 +66,12 @@ int main() {
       }
       while ((harmonics_F1 + 1) * f1 <= MAX_FREQ) harmonics_F1++; //afegeix tants harmonics com sigui possible de f1 (sense superar "harmonics") a "harmonics_F1"
       while ((harmonics_F2 + 1) * f2 <= MAX_FREQ) harmonics_F2++; //afegeix tants harmonics com sigui possible de f2 (sense superar "harmonics") a "harmonics_F2"
+      double F1[harmonics_F1][2], F2[harmonics_F2][2];
       nota_musical(f1, harmonics_F1, potencia, F1);
       nota_musical(f2, harmonics_F2, potencia, F2);
       printf("\nDissonancia: %.16G\n", disso_complexa(harmonics, harmonics, F1, F2));
       return 1;
+    }
     default:
       printf("No s'ha introduit un valor correcte.\n");
       return 1;
