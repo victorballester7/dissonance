d7451a86acef7640a2d8cd7194e53ca1bab4cac4
Carlo Sala <carlosalag@protonmail.com>
2021-06-03 19:41:10 +0200

    millorant script per case 4

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 gcc -Wall -pedantic -o dissonance dissonance.c -lm
 ./dissonance
-if [[ $? -eq 0 ]]; then
+returnvalue=$?
+if [[ $returnvalue -eq 0 ]]; then
   gnuplot -p -e "plot [1:2] 'diss.txt' with lines"
+elif [[ $returnvalue -eq 4 ]]; then
+  gnuplot -p -e "plot [60:2000] 'diss.txt' with lines"
 fi
